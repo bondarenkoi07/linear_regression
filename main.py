9a69ed479f5f6712ca7faad7a2e006a47c0f476c
bondaenko.ilya <bondaenko.ilya@wb.ru>
2023-03-05 02:37:06 +0300

    add graphs and annotations

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
+import matplotlib.pyplot as plt
 import numpy as np
 from numpy import linalg
 
 from mock_dataset import dataset
 
-A = np.array([
-    [1, x] for x in dataset.X
-])
+A = np.array([[1, x] for x in dataset.X])
 
-weights = linalg.inv(np.dot(A.transpose(), A)).dot(A.transpose()).dot(dataset.Y)
+weights = linalg.inv(np.dot(A.transpose(), A)).dot(
+    A.transpose().dot(dataset.Y)
+)  # веса на основе эталонных значений
 
-restored_Y = A.dot(weights)
+restored_Y = A.dot(weights)  # восстановление зависимой переменной
 
-rrv = dataset.Y - restored_Y
+rrv = dataset.Y - restored_Y  # вектор регрессионных остатков
 
-sse = rrv.dot(rrv.transpose())
+sse = rrv.dot(rrv.transpose())  # сумма квадратов регрессионных остатков
 
-print(sse)
+mse = sse / len(dataset.Y)  # усредненнная ошибка
+
+plt.plot(dataset.X, dataset.Y, label="original func value", color="g")
+plt.plot(dataset.X, restored_Y, label="linear regression", color="b")
+plt.ylabel("dependent variable")
+plt.xlabel("independent variable")
+
+plt.legend()
+
+plt.show()
+
+print(f"mse={mse} sse={sse}")
--- mock_dataset/dataset.py
+++ mock_dataset/dataset.py
@@ -1,3 +1,5 @@
+""" Набор данных для одномерной регрессии"""
+
 import math
 import random
 
@@ -5,7 +7,8 @@ import numpy as np
 
 
 def __analytic_func__(x):
-    return math.log(x) + 3 * math.sin(x) + random.lognormvariate(0, 1)
+    """ Функция, определяющая зависимость набора данных Y от X @returns dependent value"""
+    return math.log(x) + 3 * math.sin(x) + random.lognormvariate(0, 1) % 10
 
 
 X = np.arange(0.001, 600, 0.001)

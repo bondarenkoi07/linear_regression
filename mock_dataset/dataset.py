da032c0e32b2b3b66906dcd68d13681563f10cd3
bondaenko.ilya <bondaenko.ilya@wb.ru>
2023-03-05 02:45:16 +0300

    drop meaningless values

--- mock_dataset/dataset.py
+++ mock_dataset/dataset.py
@@ -8,7 +8,7 @@ import numpy as np
 
 def __analytic_func__(x):
     """ Функция, определяющая зависимость набора данных Y от X @returns dependent value"""
-    return math.log(x) + 3 * math.sin(x) + random.lognormvariate(0, 1) % 10
+    return math.log(x) + 3 * math.sin(x)
 
 
 X = np.arange(0.001, 600, 0.001)
